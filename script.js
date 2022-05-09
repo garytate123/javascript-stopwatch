5bbe84da70e6cb72d8b5f47556bc90aad40b187c
garytatelufc@gmail.com <garytatelufc@gmail.com>
2022-05-09 20:58:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+// JavaScript Stopwacth by Gary Tate
+var mins = 00;
+var seconds = 00; 
+var tens = 00; 
+var appendTens = document.getElementById("tens");
+var appendSeconds = document.getElementById("seconds");
+var appendMins = document.getElementById("mins");
+var buttonStart = document.getElementById('button-start');
+var buttonStop = document.getElementById('button-stop');
+var buttonReset = document.getElementById('button-reset');
+var Interval ;
+
+buttonStart.onclick = function() {
+  
+   clearInterval(Interval);
+   Interval = setInterval(startTimer, 10);
+}
+
+  buttonStop.onclick = function() {
+     clearInterval(Interval);
+}
+
+
+buttonReset.onclick = function() {
+   clearInterval(Interval);
+  tens = "00";
+    seconds = "00";
+    mins = "00";
+  appendTens.innerHTML = tens;
+    appendSeconds.innerHTML = seconds;
+    appendMins.innerHTML = mins;
+}
+
+ 
+
+function startTimer () {
+  tens++; 
+  
+  if(tens < 9){
+    appendTens.innerHTML = "0" + tens;
+  }
+  
+  if (tens > 9){
+    appendTens.innerHTML = tens;
+    
+  } 
+  
+  if (tens > 99) {
+    console.log("seconds");
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = 0;
+    appendTens.innerHTML = "0" + 0;
+  }
+  
+if (seconds > 59) {
+  console.log("min");
+  mins++
+  appendMins.innerHTML = "0" + mins;
+  seconds = 0;
+  appendSeconds.innerHTML = "0" + 0;
+}
+
+  if (seconds > 9){
+    appendSeconds.innerHTML = seconds;
+  }
+
+}
+
+
